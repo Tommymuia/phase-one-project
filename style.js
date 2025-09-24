1535488638caf92b140f5bce122f7ccf1d476b1f
Tommymuia <muiathomas.mt@gmail.com>
2025-09-24 14:25:59 +0300

    added

--- style.js
+++ style.js
@@ -1,25 +1,22 @@
-const API_URL ="http://localhost:3000/tickets"
-const forms =document.getElementById("ticketForm");
-const searchBox= document.getElementById("searchBox");
-const tbody= document.getElementById("ticketsTable");
-
+const API_URL = "http://localhost:3000/tickets";
+const forms = document.getElementById("ticketForm");
+const searchBox = document.getElementById("searchBox");
+const tbody = document.getElementById("ticketsTable");
 
 //fetching data from the Json file
 
 async function fetchTickets() {
-    const res = await fetch(API_URL)
-    const data= await res.json();
-    renderTickets(data);
-    
+  const res = await fetch(API_URL);
+  const data = await res.json();
+  renderTickets(data);
 }
 //render tickets
-function renderTickets(tickets){
-    ticketsTable.innerHTML="";
-    tickets.forEach(ticket => {
-        const row =document.createElement("tr");
-        row.classList.add("border-b");
-        row.innerHTML=
-        `
+function renderTickets(tickets) {
+  ticketsTable.innerHTML = "";
+  tickets.forEach((ticket) => {
+    const row = document.createElement("tr");
+    row.classList.add("border-b");
+    row.innerHTML = `
         <td class="py-2 px-4"> ${ticket.firstName}</td>
          <td class="py-2 px-4"> ${ticket.lastName}</td>
           <td class="py-2 px-4"> ${ticket.email}</td>
@@ -31,7 +28,19 @@ function renderTickets(tickets){
              
              
              </td>
-        `
-        
-    });
+             ;
+             ticketsTable.appendChild("row")
+             ;`;
+  });
 }
+// adding tickets to the UI
+ticketForm.addEventlistener("submit",  async (e) =>{
+    e.preventDefault();
+    const newTicket={
+        firstName:document.getElementById("firstName").value,
+        lastName:document.getElementById("lastName").value,
+        email:document.getElementById("email").value,
+        ticketCount:document.parseInt(getElementById("ticketCount").value, 10)
+
+    }
+})
