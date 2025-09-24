7dd3cdd978b744c495260fbdd2f0dfbfe61fb4b8
Tommymuia <muiathomas.mt@gmail.com>
2025-09-24 15:24:10 +0300

    added

--- style.js
+++ style.js
@@ -1,7 +1,7 @@
 const API_URL = "http://localhost:3000/tickets";
-const forms = document.getElementById("ticketForm");
+const ticketForm = document.getElementById("ticketForm");
 const searchBox = document.getElementById("searchBox");
-const tbody = document.getElementById("ticketsTable");
+const ticketsTable = document.getElementById("ticketsTable");
 
 //fetching data from the Json file
 
@@ -22,25 +22,39 @@ function renderTickets(tickets) {
           <td class="py-2 px-4"> ${ticket.email}</td>
            <td class="py-2 px-4"> ${ticket.ticketCount}</td>
             <td class="py-2 px-4"> ${ticket.action}</td>
-             <td class="py-2 px-4" space-x-2>
-             <button onclick"editTicket(${ticket.id})" class="bg-green-400 text-white px-2 py-1 rounded "> Edit</button>
-             <button onclicl"deleteTicket(${ticket.id})" class="bg-red-400 text-white px-2 py-1 rounded"> Delete</button>
+            
+             <td class="py-2 px-4 space-x-2">
+
+             <button onclick="editTicket(${ticket.id})" class="bg-green-400 text-white px-2 py-1 rounded "> Edit</button>
+             <button onclick="deleteTicket(${ticket.id})" class="bg-red-400 text-white px-2 py-1 rounded"> Delete</button>
              
              
              </td>
-             ;
-             ticketsTable.appendChild("row")
-             ;`;
+
+             `;
+
+     ticketsTable.appendChild(row)
   });
 }
 // adding tickets to the UI
-ticketForm.addEventlistener("submit",  async (e) =>{
-    e.preventDefault();
-    const newTicket={
-        firstName:document.getElementById("firstName").value,
-        lastName:document.getElementById("lastName").value,
-        email:document.getElementById("email").value,
-        ticketCount:document.parseInt(getElementById("ticketCount").value, 10)
+async function addTicket(e) {
+  e.preventDefault();
+  const newTicket = {
+    firstName: document.getElementById("firstName").value,
+    lastName: document.getElementById("lastName").value,
+    email: document.getElementById("email").value,
+    ticketCount: parseInt(document.getElementById("ticketCount").value, 10),
+  };
 
-    }
-})
+  await fetch(API_URL, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(newTicket),
+  });
+
+  ticketForm.reset();
+  fetchTickets();
+}
+
+ticketForm.addEventListener("submit", addTicket);
+fetchTickets();
