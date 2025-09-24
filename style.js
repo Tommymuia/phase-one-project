87372a274fe1c009df37d24ff06f40b330c1618a
Tommymuia <muiathomas.mt@gmail.com>
2025-09-24 14:08:37 +0300

    added

---
+++ style.js
@@ -0,0 +1,37 @@
+const API_URL ="http://localhost:3000/tickets"
+const forms =document.getElementById("ticketForm");
+const searchBox= document.getElementById("searchBox");
+const tbody= document.getElementById("ticketsTable");
+
+
+//fetching data from the Json file
+
+async function fetchTickets() {
+    const res = await fetch(API_URL)
+    const data= await res.json();
+    renderTickets(data);
+    
+}
+//render tickets
+function renderTickets(tickets){
+    ticketsTable.innerHTML="";
+    tickets.forEach(ticket => {
+        const row =document.createElement("tr");
+        row.classList.add("border-b");
+        row.innerHTML=
+        `
+        <td class="py-2 px-4"> ${ticket.firstName}</td>
+         <td class="py-2 px-4"> ${ticket.lastName}</td>
+          <td class="py-2 px-4"> ${ticket.email}</td>
+           <td class="py-2 px-4"> ${ticket.ticketCount}</td>
+            <td class="py-2 px-4"> ${ticket.action}</td>
+             <td class="py-2 px-4" space-x-2>
+             <button onclick"editTicket(${ticket.id})" class="bg-green-400 text-white px-2 py-1 rounded "> Edit</button>
+             <button onclicl"deleteTicket(${ticket.id})" class="bg-red-400 text-white px-2 py-1 rounded"> Delete</button>
+             
+             
+             </td>
+        `
+        
+    });
+}
