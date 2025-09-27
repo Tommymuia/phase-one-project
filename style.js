4b2b324ac3dd12cbc4262416d2922fbde2548685
Tommymuia <muiathomas.mt@gmail.com>
2025-09-27 11:42:12 +0300

    added

--- style.js
+++ style.js
@@ -37,7 +37,7 @@ function renderTickets(tickets) {
 
 async function handleFormSubmit(e) {
   e.preventDefault();
-  
+
   const ticketData = {
     firstName: document.getElementById("firstName").value,
     lastName: document.getElementById("lastName").value,
@@ -66,7 +66,7 @@ async function handleFormSubmit(e) {
   submitButton.textContent = "ADD TICKET";
   submitButton.classList.remove("bg-green-500", "hover:bg-green-600");
   submitButton.classList.add("bg-blue-500", "hover:bg-blue-600");
-  
+
   fetchTickets();
 }
 
@@ -74,42 +74,41 @@ ticketForm.addEventListener("submit", handleFormSubmit);
 fetchTickets();
 
 async function deleteTicket(id) {
-    const confirmed = confirm("Seriously, are you sure you want to delete the ticket?");
-    if(!confirmed){
-        return;
-    } 
-    
-try{
-    const res = await fetch(`${API_URL}/${id}`, {
-        method: "DELETE",
+  const confirmed = confirm(
+    "Seriously, are you sure you want to delete the ticket?"
+  );
+  if (!confirmed) {
+    return;
+  }
 
-    })
-    if(res.ok){
-        fetchTickets();
-    }else{
-        console.error("Failed to delete tickets:", res.status, res.statusText);
-        alert("error: Could not delete ticket");
+  try {
+    const res = await fetch(`${API_URL}/${id}`, {
+      method: "DELETE",
+    });
+    if (res.ok) {
+      fetchTickets();
+    } else {
+      console.error("Failed to delete tickets:", res.status, res.statusText);
+      alert("error: Could not delete ticket");
     }
-    
-}catch (error) {
+  } catch (error) {
     console.error("Error during delete operation:", error);
     alert("Network error: Could not reach the server.");
   }
 }
 
-async function editTicket(id){
-    const res = await fetch(`${API_URL}/${id}`);
-    const ticketToEdit = await res.json();
+async function editTicket(id) {
+  const res = await fetch(`${API_URL}/${id}`);
+  const ticketToEdit = await res.json();
 
-    document.getElementById("firstName").value = ticketToEdit.firstName;
-    document.getElementById("lastName").value = ticketToEdit.lastName;
-    document.getElementById("email").value = ticketToEdit.email;
-    document.getElementById("ticketCount").value = ticketToEdit.ticketCount;
-    
-    toEditId = id;
-    const submitButton = ticketForm.querySelector('button[type="submit"]');
-    submitButton.textContent = "UPDATE TICKET";
-    submitButton.classList.remove("bg-blue-500", "hover:bg-blue-600");
-    submitButton.classList.add("bg-green-500", "hover:bg-green-600");
+  document.getElementById("firstName").value = ticketToEdit.firstName;
+  document.getElementById("lastName").value = ticketToEdit.lastName;
+  document.getElementById("email").value = ticketToEdit.email;
+  document.getElementById("ticketCount").value = ticketToEdit.ticketCount;
+
+  toEditId = id;
+  const submitButton = ticketForm.querySelector('button[type="submit"]');
+  submitButton.textContent = "UPDATE TICKET";
+  submitButton.classList.remove("bg-blue-500", "hover:bg-blue-600");
+  submitButton.classList.add("bg-green-500", "hover:bg-green-600");
 }
-
